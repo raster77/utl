4da539b24c4ce8eee9c8747b6fb006d9fbf96e6a
thierry <thdi.4449@gmail.com>
2021-02-14 07:24:36 +0100

    Format sources and add UtlChrono

--- src/utlRandom.hpp
+++ src/utlRandom.hpp
@@ -8,22 +8,22 @@ static std::mt19937 rndEngine(std::chrono::high_resolution_clock::now().time_sin
 
 namespace utl
 {
-namespace random
-{
+    namespace random
+    {
 
-int getUniformInt(const int min, const int max)
-{
-    std::uniform_int_distribution<int> dist(min, max);
-    return dist(rndEngine);
-}
+        int getUniformInt(const int min, const int max)
+        {
+            std::uniform_int_distribution<int> dist(min, max);
+            return dist(rndEngine);
+        }
 
-float getUniformFloat(const float min, const float max)
-{
-    std::uniform_real_distribution<float> dist(min, max);
-    return dist(rndEngine);
-}
+        float getUniformFloat(const float min, const float max)
+        {
+            std::uniform_real_distribution<float> dist(min, max);
+            return dist(rndEngine);
+        }
 
-}
+    }
 }
 
 #endif // UTLRANDOM_HPP
--- src/utlTime.hpp
+++ src/utlTime.hpp
@@ -12,21 +12,21 @@
 
 namespace utl
 {
-namespace time
-{
-    
-    std::chrono::time_point<clock> getTimePoint()
+    namespace time
     {
-        return clock::now();
-    }
+        
+        std::chrono::time_point<clock> getTimePoint()
+        {
+            return clock::now();
+        }
 
-    template <typename Duration = std::milli>
-    double elapsedTime(const std::chrono::time_point<clock>& start,
-        const std::chrono::time_point<clock>& end)
-    {
-        return std::chrono::duration<double, Duration>(end - start).count();
+        template <typename Duration = std::milli>
+        double elapsedTime(const std::chrono::time_point<clock>& start,
+            const std::chrono::time_point<clock>& end)
+        {
+            return std::chrono::duration<double, Duration>(end - start).count();
+        }
     }
-}
 }
 
 #endif // UTLRANDOM_HPP
